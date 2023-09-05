cea9f8c355883816beec01443f9f86538c547b08
GershonGolubitsky <genomishe@gmail.com>
2023-09-05 14:20:45 +0300

    added all files

---
+++ RandomWalk.py
@@ -0,0 +1,7 @@
+import  random
+import matplotlib
+class randomWalk:
+    def _init_(self, dimensions, steps):
+        self.d = dimensions
+        self.steps = steps
+        self.cordinate=[[0 for step in range]]
