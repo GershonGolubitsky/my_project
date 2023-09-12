521ec1c709a338e8d74860e74bff6835c6833341
GershonGolubitsky <genomishe@gmail.com>
2023-09-12 12:03:59 +0300

    trial2

--- RandomWalk.py
+++ RandomWalk.py
@@ -2,6 +2,6 @@ import  random
 import matplotlib
 class randomWalk:
     def _init_(self, dimensions, steps):
-        self.d = dimensions
+        self.d = dimehnsions
         self.steps = steps
         self.cordinate=[[0 for step in range]]
