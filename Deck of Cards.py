92592d37e40a113cd07d801dcf6d7804c8b57ca9
GershonGolubitsky <genomishe@gmail.com>
2023-10-23 14:10:56 +0300

    Final program and add all comments

--- Deck of Cards.py
+++ Deck of Cards.py
@@ -21,6 +21,7 @@ class Card:
         if self.rank == other.rank:
             return self.suit > other.suit
         return self.rank > other.rank
+
 # Create a `Deck` class to represent a standard deck of cards
 class DeckCards():
     def __init__(self):
@@ -37,6 +38,7 @@ class DeckCards():
             for name in range(len(self.all_name)):
                 #Create a card by calling the constructor of the parent class
                 one_card = Card(suit, self.all_name[name], name + 1)
+                # Adds a card to the deck
                 self.deck.append(one_card)
         # Adding jokers to the deck
         for i in range(2):
@@ -79,11 +81,14 @@ def deal_hand(deck, num_cards):
     hand = ""
     for i in range(num_cards):
         hand += deck.draw()
+    # Returns all the cards that came out of the deck
     return str(hand)
 
 # count how many cards of each rank are in the deck
 def count_cards(deck):
+    # Resets a deck
     count = {'A': 0, '2': 0, '3': 0, '4': 0, '5': 0, '6': 0, '7': 0, '8': 0, '9': 0, '10': 0, 'Prince': 0, 'Queen': 0, 'King': 0, 'Joker': 0}
+    # If a card is in the deck, add 1 to the count
     for card in deck.deck:
         name = str(card.name)
         count[name] += 1
@@ -122,9 +127,3 @@ if __name__ == "__main__":
     # print(str(one_card))
     # print(str(a))
     # print(f"Current card: {a[5]}")
-# print(a.shuffle())
-# יצירת חפיסת קלפים
-# deck = DeckCards()
-
-# הדפסת כל הקלפים בחפיסה
-# deck.print_deck()
